949b13a85a7663229f56496dbbae2922baa65235
Thoushif Aazam Shaik <aazam555@gmail.com>
2024-04-30 11:29:57 -0500

    added code for sums

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { ApiClient, ApiError } from "./api";
 
-export { ApiClient, ApiError };
+import { sum as simpleSUm } from "./demo1/demo1";
+import { sum as sumwithargs } from "./demo2/demo2";
+
+export { ApiClient, ApiError, simpleSUm, sumwithargs };
